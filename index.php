195ed8fd5d16bdee4254377a032d2137dfa1e6b8
farhanisty <farhannivtasija@gmail.com>
2024-05-12 18:48:45 +0530

    make index dynamic and add logout button

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
+
+session_start();
+
+require_once(__DIR__ . "/facades/auth.php");
+
+if (!isLogged()) {
+  require_once(__DIR__ . "/facades/route.php");
+  redirect("login.php");
+}
+
 $title = "home";
+
 include(__DIR__ . "/templates/header.php");
+
 ?>
-<h1>hello world</h1>
+<h1>hello <?= $_SESSION["username"] ?></h1>
+<a href="logout.php">logout</a>
+
 <?php include(__DIR__ . "/templates/footer.php"); ?>
